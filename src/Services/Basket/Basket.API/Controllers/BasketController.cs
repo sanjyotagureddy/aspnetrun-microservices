9b84f62d4e22eb6d00e04266c7ef94462c595591
Sanjyot Agureddy <sanjyot.agureddy@hotmail.com>
2021-04-11 22:24:55 +0530

    - Dockerized Discount.Grpc

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -41,7 +41,6 @@ namespace Basket.API.Controllers
             {
                 var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
                 item.Price -= coupon.Amount;
-
             }
 
             return Ok(await _repository.UpdateBasket(basket));
