a33ff867fc058a31a00e54b5a95597945fa452b0
Sanjyot Agureddy <sanjyot.agureddy@hotmail.com>
2021-04-13 00:23:27 +0530

    - Added OrderController

---
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.DeleteCommand;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+
+namespace Ordering.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet("{userName}", Name = "GetOrder")]
+        [ProducesResponseType(typeof(IEnumerable<OrdersVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrdersVm>>> GetOrdersByUserName(string userName)
+        {
+            var query = new GetOrdersListQuery(userName);
+            var orders = await _mediator.Send(query);
+            return Ok(orders);
+        }
+
+        // testing purpose
+        [HttpPost(Name = "CheckoutOrder")]
+        [ProducesResponseType(typeof(int),StatusCodes.Status200OK)]
+        public async Task<ActionResult<int>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut(Name = "UpdateOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateOrder([FromBody] UpdateOrderCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            var command = new DeleteOrderCommand() { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
