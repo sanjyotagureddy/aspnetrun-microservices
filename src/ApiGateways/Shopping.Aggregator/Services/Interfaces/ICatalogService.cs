280dc94b612e694b3e7194f9ef700583566f92c3
Sanjyot Agureddy <sanjyot.agureddy4@gmail.com>
2021-04-17 20:07:15 +0530

    - Developed Shopping.Aggregator API

---
+++ src/ApiGateways/Shopping.Aggregator/Services/Interfaces/ICatalogService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+        Task<IEnumerable<CatalogModel>> GetProductByName(string name);
+        Task<CatalogModel> GetCatalog(string id);
+    }
+}
