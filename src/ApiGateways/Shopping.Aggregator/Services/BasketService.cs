9f237ef4934bc42fdbd7815016fc50e639785f68
Sanjyot Agureddy <sanjyot.agureddy4@gmail.com>
2021-04-17 23:29:46 +0530

    - Developed Shopping.Aggregator API (#20) (#21)

---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services.Interfaces;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _httpClient.GetAsync($"/api/v1/Basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<BasketModel> GetBasket(string userName);
+    }
+}
